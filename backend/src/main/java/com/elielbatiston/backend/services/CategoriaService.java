93e2a7bf2bab94af37c442d3b9294c781dff0c7b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-15 20:45:13 -0300

    Deletando uma Categoria com DELETE

--- backend/src/main/java/com/elielbatiston/backend/services/CategoriaService.java
+++ backend/src/main/java/com/elielbatiston/backend/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.elielbatiston.backend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.elielbatiston.backend.domain.Categoria;
 import com.elielbatiston.backend.repositories.CategoriaRepository;
+import com.elielbatiston.backend.services.exception.DataIntegrityException;
 import com.elielbatiston.backend.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		find(obj.getId());
 		
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
