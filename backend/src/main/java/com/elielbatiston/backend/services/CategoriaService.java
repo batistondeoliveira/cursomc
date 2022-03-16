3ea0478e79eb267e44fae311e2a3485efa802a53
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-15 22:05:57 -0300

    Validacao sintatica com Bean Validation

--- backend/src/main/java/com/elielbatiston/backend/services/CategoriaService.java
+++ backend/src/main/java/com/elielbatiston/backend/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.elielbatiston.backend.domain.Categoria;
+import com.elielbatiston.backend.dto.CategoriaDTO;
 import com.elielbatiston.backend.repositories.CategoriaRepository;
 import com.elielbatiston.backend.services.exception.DataIntegrityException;
 import com.elielbatiston.backend.services.exception.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
