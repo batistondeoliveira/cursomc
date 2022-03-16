d8aa13ae3948fe2f944c7017c753e3a2d619d605
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-15 22:18:35 -0300

    Personalizando o retorno da validacao

--- backend/src/main/java/com/elielbatiston/backend/dto/CategoriaDTO.java
+++ backend/src/main/java/com/elielbatiston/backend/dto/CategoriaDTO.java
@@ -13,9 +13,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-
-	@NotEmpty(message="Preenchimento obrigatório")
+	
 	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String nome;
 
 	public CategoriaDTO() {	
