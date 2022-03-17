a8c4a9bb56ea6b37fba412a129e1b8bec9756ec3
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-16 21:02:24 -0300

    Validacao customizada: email nao repetido na insercao de cliente

--- backend/src/main/java/com/elielbatiston/backend/repositories/ClienteRepository.java
+++ backend/src/main/java/com/elielbatiston/backend/repositories/ClienteRepository.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.elielbatiston.backend.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Serializable> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- backend/src/main/java/com/elielbatiston/backend/services/validation/ClienteInsertValidator.java
+++ backend/src/main/java/com/elielbatiston/backend/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.elielbatiston.backend.domain.Cliente;
 import com.elielbatiston.backend.domain.enums.TipoCliente;
 import com.elielbatiston.backend.dto.ClienteNewDTO;
+import com.elielbatiston.backend.repositories.ClienteRepository;
 import com.elielbatiston.backend.resources.exception.FieldMessage;
 import com.elielbatiston.backend.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
