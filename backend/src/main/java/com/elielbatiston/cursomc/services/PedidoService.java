1e51f433213c2b5c70bdbb6df536e16f375915cd
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-20 15:10:56 -0300

    Alterado o nome do pacote de backend para cursomc

---
+++ backend/src/main/java/com/elielbatiston/cursomc/services/PedidoService.java
@@ -0,0 +1,78 @@
+package com.elielbatiston.cursomc.services;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.elielbatiston.cursomc.domain.ItemPedido;
+import com.elielbatiston.cursomc.domain.PagamentoComBoleto;
+import com.elielbatiston.cursomc.domain.Pedido;
+import com.elielbatiston.cursomc.domain.enums.EstadoPagamento;
+import com.elielbatiston.cursomc.repositories.ItemPedidoRepository;
+import com.elielbatiston.cursomc.repositories.PagamentoRepository;
+import com.elielbatiston.cursomc.repositories.PedidoRepository;
+import com.elielbatiston.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;	
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		emailService.sendOrderConfirmationHtmlEmail(obj);
+		
+		return obj;
+	}
+}
---
+++ backend/src/main/java/com/elielbatiston/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,54 @@
+package com.elielbatiston.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.elielbatiston.cursomc.domain.Cliente;
+import com.elielbatiston.cursomc.domain.enums.TipoCliente;
+import com.elielbatiston.cursomc.dto.ClienteNewDTO;
+import com.elielbatiston.cursomc.repositories.ClienteRepository;
+import com.elielbatiston.cursomc.resources.exception.FieldMessage;
+import com.elielbatiston.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+
+		return list.isEmpty();
+	}
+}
