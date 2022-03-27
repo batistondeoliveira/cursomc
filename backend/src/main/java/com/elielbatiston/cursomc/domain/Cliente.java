6f4918e9bfc19fbcea58a628929e309b0c3f77a3
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-27 15:40:54 -0300

    Usando padrao de nomes para imagens

--- backend/src/main/java/com/elielbatiston/cursomc/domain/Cliente.java
+++ backend/src/main/java/com/elielbatiston/cursomc/domain/Cliente.java
@@ -57,9 +57,7 @@ public class Cliente implements Serializable {
 	
 	@ElementCollection(fetch=FetchType.EAGER)
 	@CollectionTable(name="PERFIS")
-	private Set<Integer> perfis = new HashSet<>();
-	
-	private String imageUrl;
+	private Set<Integer> perfis = new HashSet<>();	
 		
 	public Cliente() {	
 		addPerfil(Perfil.CLIENTE);
@@ -159,15 +157,7 @@ public class Cliente implements Serializable {
 
 	public void addPerfil(Perfil perfil) {
 		this.perfis.add(perfil.getCod());
-	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+	}	
 
 	@Override
 	public boolean equals(Object obj) {
