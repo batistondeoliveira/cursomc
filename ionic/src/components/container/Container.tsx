71e0323365382328e3364353c32edd3f150122f8
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-03-31 22:38:31 -0300

    Criado componente Container

---
+++ ionic/src/components/container/Container.tsx
@@ -0,0 +1,25 @@
+import { IonContent, IonPage } from "@ionic/react";
+import Header, { HeaderProps } from "components/header/Header";
+
+export interface ContainerProps {
+  header: HeaderProps;
+  children: any;
+}
+
+const Container: React.FC<ContainerProps> = ({ header, children }: ContainerProps) => {
+  const { title, to } = header;
+
+  return (
+    <IonPage>
+      <Header title={title} to={to} />
+
+      <IonContent fullscreen>
+        <div style={{padding: '0px 10px'}}>
+          {children}
+        </div>
+      </IonContent>    
+    </IonPage>
+  )
+}
+
+export default Container;
